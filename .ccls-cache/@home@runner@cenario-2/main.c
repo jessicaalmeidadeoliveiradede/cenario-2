8356819b83a63c2e0263f4cda5b0e942555a14f0
Jéssica Almeida de Oliveira <32886156+jessicaalmeidadeoliveiradede@users.noreply.github.com>
2023-04-03 20:39:38 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@cenario-2/main.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+struct list{
+   int numero;
+  struct numero list;
+};
+typedef struct list numero;
+
+int tamanho 100;
+
+
+int main(void) {
+  void list(numero){
+    insere(6);
+    insere(7);
+    insere(6);
+    insere(9);
+    insere(1);
+    insere(6);
+    insere(7);
+    insere(6);
+    insere(9);
+  };
+  printf("insere os numero na lista",insere);
+  void list(numero){
+   imprime(1);
+    imprime(9);
+    imprime(6);
+    imprime(7);
+    imprime(6);
+    imprime(1);
+    imprime(9);
+      imprime(6);
+     imprime(7);
+    imprime(6);
+      
+
+    
+  };
+  printf("imprime os numeros acima ",imprime);
+  void list(numero){
+        imprime(8); 
+        imprime(1);
+        imprime(9);
+        imprime(6);
+        imprime(7);
+        imprime(6);
+        imprime(1);
+        imprime(9);
+        imprime(6);
+        imprime(7); 
+         imprime(6);
+    
+  }
+  print("imprime os numeros da lista acima\n",imprime);
+  void list(numerio){
+    remove(1);
+    remove(9);
+    remove(6);
+    remove(7);
+    remove(6);
+  
+  }
+  
+  printf("remove uma lista de numero e retona lista fazia\n",lista,fazia);
+  return 0;
+}
